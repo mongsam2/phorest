3f6f2b06ca949ea830ca90d96c1848022ac733da
mongsam2 <songjaehyun121476@gmail.com>
2024-07-28 16:00:23 +0900

    카테고리 API 완성

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/categories/', include("categories.urls")),
+    path("api/galleries/", include("galleries.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/galleries/views.py
+++ backend/galleries/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import GalleryListSerializer
+from rest_framework.response import Response
+
+from .models import Gallery
+
+from rest_framework.exceptions import ParseError
 
 # Create your views here.
+class Galleries(APIView):
+
+    def get(self, request):
+        type = request.query_params.get("type")
+        category = request.query_params.get("category")
+        page = request.query_params.get("page", "1")
+        if not type:
+            raise ParseError("타입 정보가 입력되지 않았습니다.")
+        if not category:
+            raise ParseError("카테고리 정보가 입력되지 않았습니다.")
+        
+        galleries = Gallery.objects.filter(type__name=type, category__name=category)
+        serializer = GalleryListSerializer(galleries, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        pass
