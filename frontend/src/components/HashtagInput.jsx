a5b645142a1e61665f3f03580f62377972a74730
HiImSUN <kshk08@hanyang.ac.kr>
2024-08-05 02:29:43 +0900

    모달창에 해시태그 구현

--- frontend/src/components/HashtagInput.jsx
+++ frontend/src/components/HashtagInput.jsx
@@ -1,80 +1,148 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import cancelButton from "../assets/btn_cancel.png";
 
-function HashtagInput({ hashtags, setHashtags }) {
+const HashTagInputWrapper = styled.div`
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 90%;
+`;
+
+const Input = styled.input`
+  margin-left: 80px;
+  padding: 8px;
+  font-size: 16px;
+  border: 2px solid #2d2d2d29;
+  border-radius: 4px;
+  width: 100%;
+  margin-bottom: 5px;
+  box-sizing: border-box;
+  &:focus {
+    border-color: #54c1ff;
+    outline: none;
+  }
+`;
+
+const Error = styled.p`
+  margin-left: 80px;
+  color: red;
+  font-size: 14px;
+  text-align: left;
+  width: 100%;
+`;
+
+const TagList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 5px;
+  align-items: flex-start;
+  align-content: center;
+  width: 90%;
+`;
+
+const Tag = styled.span`
+  background-color: transparent;
+  border: 1px solid #d9d9d9;
+  color: #000000;
+  padding: 10px 15px;
+  border-radius: 50px;
+  margin-right: 10px;
+  margin-bottom: 5px;
+  display: flex;
+  align-items: center; /* Centering text vertically */
+  font-family: Noto Sans KR;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 16px;
+  letter-spacing: -1px;
+`;
+
+const RemoveButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
+
+const HashTagInput = ({ initialHashtags, onHashtagsChange }) => {
+  const [hashtags, setHashtags] = useState(initialHashtags);
   const [input, setInput] = useState("");
   const [error, setError] = useState("");
 
+  useEffect(() => {
+    setHashtags(initialHashtags);
+  }, [initialHashtags]);
+
   const handleInputChange = (e) => {
-    setInput(e.target.value);
+    const value = e.target.value;
+    // 한글, 영어, 숫자만을 허용하는 정규 표현식
+    const regex = /^[a-zA-Zㄱ-ㅎ가-힣0-9]*$/;
+
+    // 입력값이 유효하면 상태를 업데이트
+    if (regex.test(value)) {
+      setInput(value);
+      setError("");
+    } else {
+      setError("한글, 영어, 숫자만 입력 가능합니다.");
+    }
   };
 
   const handleKeyDown = (e) => {
-    if (e.key === "Enter" || e.key === " ") {
+    if (e.key === " " || e.key === "Enter") {
       e.preventDefault();
-      handleAddHashtag();
-      setInput("");
+      addHashtag(input.trim());
     }
   };
 
-  const handleAddHashtag = () => {
-    const trimmedInput = input.trim();
-    if (trimmedInput === "") {
-      setError("Hashtag cannot be empty");
+  const addHashtag = (tag) => {
+    if (!tag) {
+      setError("해시태그를 다시 작성해주세요");
       return;
     }
-    if (trimmedInput.includes(" ")) {
-      setError("Hashtag cannot contain spaces");
-      return;
-    }
-    if (hashtags.includes(trimmedInput)) {
-      setError("Hashtag already exists");
+    if (hashtags.includes(tag)) {
+      setError("해당 해시태그가 이미 존재합니다");
       return;
     }
     if (hashtags.length >= 3) {
-      setError("You can only add up to 3 hashtags");
+      setError("해시태그는 최대 3개까지 추가할 수 있습니다.");
       return;
     }
-    setHashtags([...hashtags, trimmedInput]);
+    const newHashtags = [...hashtags, tag];
+    setHashtags(newHashtags);
     setInput("");
     setError("");
+    onHashtagsChange(newHashtags);
   };
 
-  const handleDeleteHashtag = (tag) => {
-    setHashtags(hashtags.filter((hashtag) => hashtag !== tag));
+  const removeHashtag = (tag) => {
+    const newHashtags = hashtags.filter((hashtag) => hashtag !== tag);
+    setHashtags(newHashtags);
+    onHashtagsChange(newHashtags);
   };
 
   return (
-    <div>
-      <div>
-        <input
-          type="text"
-          value={input}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-          placeholder="Enter a hashtag"
-          disabled={hashtags.length >= 3}
-        />
-        <button
-          type="button"
-          onClick={handleAddHashtag}
-          disabled={hashtags.length >= 3}
-        >
-          Add Hashtag
-        </button>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-      </div>
-      <div>
+    <HashTagInputWrapper>
+      <Input
+        type="text"
+        value={input}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        placeholder="Add a hashtag"
+      />
+      {error && <Error>{error}</Error>}
+      <TagList>
         {hashtags.map((tag, index) => (
-          <div key={index} style={{ display: "inline-block", margin: "5px" }}>
+          <Tag key={index}>
             #{tag}
-            <button type="button" onClick={() => handleDeleteHashtag(tag)}>
-              x
-            </button>
-          </div>
+            <RemoveButton onClick={() => removeHashtag(tag)}>
+              <img src={cancelButton} alt="x" />
+            </RemoveButton>
+          </Tag>
         ))}
-      </div>
-    </div>
+      </TagList>
+    </HashTagInputWrapper>
   );
-}
+};
 
-export default HashtagInput;
+export default HashTagInput;
--- frontend/src/components/UploadDetailModal.jsx
+++ frontend/src/components/UploadDetailModal.jsx
@@ -1,12 +1,84 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Modal, Button } from "react-bootstrap";
 import styled from "styled-components";
+import checkedIcon from "../assets/radio_checked.png";
+import uncheckedIcon from "../assets/radio_unchecked.png";
+import HashTagInput from "./HashtagInput";
 
 const BootstrapModalWrapper = styled.div`
   @import url("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css");
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap; /* 여러 줄로 배치 */
+  width: 100%; /* 전체 너비를 사용 */
+  margin: auto;
+  margin-left: 45px; /* 왼쪽 여백 조정 */
+  padding: 10px 10px; /* 좌우 여백 조정 */
+  box-sizing: border-box; /* 패딩과 보더를 너비에 포함 */
+`;
+
+const RadioItem = styled.label`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  width: 17%; /* 각 행에 5개의 항목 배치 */
+  box-sizing: border-box;
+  padding: 5px;
+  margin-bottom: 12px; /* 세로 간격 조정 */
+  margin-right: 18px; /* 오른쪽 여백 조정 */
+  div {
+    margin-left: 10px;
+    font-family: "Noto Sans KR";
+    font-size: 24px;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: -1px;
+    text-align: left;
+    white-space: nowrap;
+  }
+`;
+const RadioInput = styled.input`
+  display: none;
+  &:checked + span > img {
+    content: url(${checkedIcon});
+  }
+`;
+
+const CustomRadio = styled.span`
+  display: flex;
+  align-items: center;
+  img {
+    width: 24px;
+    height: 24px;
+    content: url(${uncheckedIcon});
+  }
+`;
+
 const UploadDetailModal = (props) => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [hashtags, setHashtags] = useState([]);
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+  const handleHashtagsChange = (newHashtags) => {
+    setHashtags(newHashtags);
+  };
+
+  const options = [
+    { value: "반려동물", label: "반려동물" },
+    { value: "바다", label: "바다" },
+    { value: "산", label: "산" },
+    { value: "캠핑", label: "캠핑" },
+    { value: "계절", label: "계절" },
+    { value: "캐릭터", label: "캐릭터" },
+    { value: "동물", label: "동물" },
+    { value: "사물", label: "사물" },
+    { value: "기타", label: "기타" },
+  ];
+
   return (
     <BootstrapModalWrapper>
       <Modal
@@ -16,17 +88,80 @@ const UploadDetailModal = (props) => {
         centered
       >
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
+          <Modal.Title
+            id="contained-modal-title-vcenter"
+            style={{
+              fontFamily: "Noto Sans KR",
+              fontSize: "24px",
+              fontWeight: "700",
+              lineHeight: "24px",
+              letterSpacing: "-1px",
+              textAlign: "left",
+              marginLeft: "30px",
+              padding: "10px",
+            }}
+          >
+            상세 정보 설정
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
+          <p
+            style={{
+              marginTop: "55px",
+              marginLeft: "55px",
+              fontFamily: "Noto Sans KR",
+              fontSize: "24px",
+              fontWeight: "700",
+              lineHeight: "24px",
+              letterSpacing: "-1px",
+              textAlign: "left",
+            }}
+          >
+            카테고리<span style={{ color: "#54C1FF" }}> (필수, 1개)</span>
           </p>
+          <Container>
+            {options.map((option, index) => (
+              <RadioItem key={index}>
+                <RadioInput
+                  type="radio"
+                  name="category"
+                  value={option.value}
+                  checked={selectedOption === option.value}
+                  onChange={handleOptionChange}
+                />
+                <CustomRadio>
+                  <img
+                    src={
+                      selectedOption === option.value
+                        ? checkedIcon
+                        : uncheckedIcon
+                    }
+                    alt={option.label}
+                  />
+                </CustomRadio>
+                <div>{option.label}</div>
+              </RadioItem>
+            ))}
+          </Container>
+
+          <p
+            style={{
+              marginTop: "70px",
+              marginLeft: "55px",
+              fontFamily: "Noto Sans KR",
+              fontSize: "24px",
+              fontWeight: "700",
+              lineHeight: "24px",
+              letterSpacing: "-1px",
+              textAlign: "left",
+            }}
+          >
+            태그<span style={{ color: "#54C1FF" }}> (선택, 3개까지)</span>
+          </p>
+          <HashTagInput
+            initialHashtags={hashtags}
+            onHashtagsChange={handleHashtagsChange}
+          />
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.onHide}>Close</Button>
